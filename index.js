048115d3f9b7c3442347473080000124d05a023a
Samuel JOSET <51910006+saxjst@users.noreply.github.com>
2019-10-27 00:41:55 +0200

    feat: create chain-fns function

---
+++ index.js
@@ -0,0 +1,9 @@
+/** Chain a list of handlers with each other
+ *
+ * @param {Function} fns List of handlers to chain
+ * @return {Function} First handler chains with the others
+ */
+const chainFns = fns =>
+  fns.reverse().reduce((nextFn, fn) => fn(nextFn), _ => _);
+
+module.exports = chainFns;
